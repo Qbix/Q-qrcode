bdc544397bce44e7f701886796d2767ff16bf29f
Greg Magarshak <greg@qbix.com>
2025-11-14 08:18:44 -0500

    Made margin: auto

--- qrcode.js
+++ qrcode.js
@@ -276,6 +276,7 @@ var QRCode;
 		function _onMakeImage() {
 			this._elImage.src = this._elCanvas.toDataURL("image/png");
 			this._elImage.style.display = "block";
+			this._elImage.style.margin = "auto";
 			this._elCanvas.style.display = "none";			
 		}
 		
